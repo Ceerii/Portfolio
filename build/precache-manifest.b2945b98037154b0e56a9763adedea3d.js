460ff58cad2e520dd56bff1eeb1a64e190cbfb79
Chiamaka Nkem-Eze <ceerieze@gmail.com>
2019-08-06 10:14:42 +0100

    pushed build

---
+++ build/precache-manifest.b2945b98037154b0e56a9763adedea3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a18b081ba4bf4314027f7906369df90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32b4bb88e32aa7123b07",
+    "url": "./static/css/main.d92309ba.chunk.css"
+  },
+  {
+    "revision": "0edb1d8ce26fbac0fc2e",
+    "url": "./static/js/2.4c90bdd4.chunk.js"
+  },
+  {
+    "revision": "32b4bb88e32aa7123b07",
+    "url": "./static/js/main.e5e3b901.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
